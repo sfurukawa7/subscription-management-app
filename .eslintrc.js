4f61c060bb3f08b105e71ee9a42a0c7cee9138ca
sfurukawa7 <shota.trp.musik711@gmail.com>
2023-01-14 18:59:12 +0900

    初期設定完了

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks", "import"],
+  rules: {
+    "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+    "func-style": ["error", "expression"],
+    "react/function-component-definition": ["warn", { namedComponents: "arrow-function" }],
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "import", next: "const" },
+      { blankLine: "always", prev: "*", next: "export" },
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "never", prev: "const", next: "const" },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index", "object", "type"],
+        pathGroups: [
+          {
+            pattern: "{react,react-dom/**,react-router-dom,next/**,@next/**}",
+            group: "builtin",
+            position: "before",
+          },
+          {
+            pattern: "@src/**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
