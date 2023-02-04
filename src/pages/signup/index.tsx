f96dccec36eca29304b9cbd79835ca1637e6a452
sfurukawa7 <shota.trp.musik711@gmail.com>
2023-02-04 09:52:14 +0900

    signUpをsignupに

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,13 @@
+import SignupForm from "@organisms/signupForm";
+
+import styles from "./styles.module.css";
+
+const SignUp = () => {
+  return (
+    <main className={styles.main}>
+      <SignupForm />
+    </main>
+  );
+};
+
+export default SignUp;
