3d669b29252fc783cc08368e0626b34929a525e2
sfurukawa7 <shota.trp.musik711@gmail.com>
2023-02-15 23:09:55 +0900

    ESLintエラー修正

--- src/utils/authContext.tsx
+++ src/utils/authContext.tsx
@@ -26,12 +26,9 @@ type AuthProviderProps = {
 export const AuthProvider = (props: AuthProviderProps) => {
   const router = useRouter();
   const [user, setUser] = useState<UserType>(null);
-  const isAvailableForViewing =
-    router.pathname === "/" ||
-    router.pathname === "/about" ||
-    router.pathname === "/login" ||
-    router.pathname === "/signup";
 
+  const isAvailableForViewing =
+    router.pathname === "/" || router.pathname === "/login" || router.pathname === "/signup";
   useEffect(() => {
     const authStateChanged = onAuthStateChanged(auth, async (user) => {
       setUser(user);
