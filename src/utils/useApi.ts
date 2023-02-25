924ad780c60a6b29ec3b9432fea74a283cc4c67c
sfurukawa7 <shota.trp.musik711@gmail.com>
2023-02-25 20:04:24 +0900

    追加画面:POSTメソッドOK!

--- src/utils/useApi.ts
+++ src/utils/useApi.ts
@@ -5,10 +5,6 @@ const axiosInstance: AxiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
     "x-api-key": process.env.NEXT_PUBLIC_AWS_X_API_KEY,
-    "Access-Control-Allow-Headers":
-      "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "DELETE, GET, OPTIONS, POST",
   },
 });
 
